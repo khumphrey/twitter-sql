b7a8c01c941ad466d704122a6cd2740093c1c001
khumphrey <khumphrey7890@gmail.com>
2016-05-02 15:13:57 -0400

    all the stuffs

---
+++ app.js
@@ -0,0 +1,53 @@
+'use strict';
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const swig = require('swig');
+const makesRouter = require('./routes');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
+const bodyParser = require('body-parser');
+const socketio = require('socket.io');
+const pg = require('pg');
+const conString = "postgres://localhost:5432/twitterdb";
+const client = new pg.Client(conString);
+
+// connect to postgres
+client.connect();
+
+// templating boilerplate setup
+app.set('views', path.join(__dirname, '/views')); // where to find the views
+app.set('view engine', 'html'); // what file extension do our templates have
+app.engine('html', swig.renderFile); // how to render html templates
+swig.setDefaults({ cache: false });
+
+// logging middleware
+app.use(morgan('dev'));
+
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
+
+
+// start the server
+var server = app.listen(1337, function(){
+  console.log('listening on port 1337');
+});
+var io = socketio.listen(server);
+
+// modular routing that uses io inside it and passes the client down to the routes
+app.use('/', makesRouter(io, client));
+
+// the typical way to use express static middleware.
+app.use(express.static(path.join(__dirname, '/public')));
+
+// // manually-written static file middleware
+// app.use(function(req, res, next){
+//   var mimeType = mime.lookup(req.path);
+//   fs.readFile('./public' + req.path, function(err, fileBuffer){
+//     if (err) return next();
+//     res.header('Content-Type', mimeType);
+//     res.send(fileBuffer);
+//   });
+// });
---
+++ routes/index.js
@@ -0,0 +1,89 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const tweetBank = require('../tweetBank.js');
+const bluebird = require('bluebird');
+
+module.exports = function makeRouterWithSockets(io, client) {
+    client.query = bluebird.promisify(client.query);
+
+    // a reusable function
+    function respondWithAllTweets(req, res, next) {
+        // client.query('SELECT * FROM tweets JOIN users ON tweets.userId = users.id', function(err, result) {
+        //     var tweets = result.rows;
+        //     res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+        // });
+        client.query('SELECT * FROM tweets JOIN users ON tweets.userId = users.id')
+            .then(result => {
+                let tweets = result.rows;
+                res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+            })
+    }
+
+    // here we basically treet the root view and tweets view as identical
+    router.get('/', respondWithAllTweets);
+    router.get('/tweets', respondWithAllTweets);
+
+    // single-user page
+    router.get('/users/:username', function(req, res, next) {
+        // client.query('SELECT * FROM tweets JOIN users ON tweets.userId = users.id WHERE name = $1', [req.params.username], function(err, result) {
+        //     var tweets = result.rows;
+        //     res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true, username: req.params.username });
+        // });
+        client.query('SELECT * FROM tweets JOIN users ON tweets.userId = users.id WHERE name = $1', [req.params.username])
+            .then(result => {
+                let tweets = result.rows;
+                res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true, username: req.params.username });
+            });
+    });
+
+    // single-tweet page
+    router.get('/tweets/:id', function(req, res, next) {
+        client.query('SELECT * FROM tweets JOIN users ON tweets.userId = users.id WHERE tweets.id = $1', [req.params.id])
+            .then(result => {
+                let tweets = result.rows;
+                res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+            });
+        // client.query('SELECT * FROM tweets JOIN users ON tweets.userId = users.id WHERE tweets.id = $1', [req.params.id], function(err, result) {
+        //     var tweets = result.rows;
+        //     res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+        // });
+    });
+
+    // create a new tweet
+    router.post('/tweets', function(req, res, next) {
+        client.query('INSERT INTO users (name) SELECT $1 WHERE NOT EXISTS (SELECT * FROM users WHERE name = $1)', [req.body.name])
+            .then(result => {
+                return client.query('INSERT INTO tweets (userid, content) VALUES ((SELECT id FROM users WHERE name = $1), $2) RETURNING *', [req.body.name, req.body.text])
+            })
+            .then(result => {
+                let newTweet = result.rows
+                io.sockets.emit('new_tweet', newTweet);
+                res.redirect('/');
+            })
+
+        // client.query('SELECT * FROM users WHERE name = $1', [req.body.name], function(error, result) {
+        //   let user = result.rows[0];
+        //   if (user) {
+        //     client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2) RETURNING *', [user.id, req.body.text], function(err, result) {
+        //         console.log('rows', result)
+        //         let newTweet = result.rows;
+        //         io.sockets.emit('new_tweet', newTweet);
+        //         res.redirect('/');
+        //     })
+        //   } else {
+
+        //   }
+        // })
+        // client.query('INSERT INTO users (name) SELECT $1 WHERE NOT EXISTS (SELECT * FROM users WHERE name = $1)', [req.body.name], function(error, result) {
+        //     client.query('INSERT INTO tweets (userid, content) VALUES ((SELECT id FROM users WHERE name = $1), $2) RETURNING *', [req.body.name, req.body.text], function(err, result) {
+        //         var newTweet = result.rows
+        //         io.sockets.emit('new_tweet', newTweet);
+        //         res.redirect('/');
+        //     })
+        // })
+
+    });
+
+    return router;
+};
